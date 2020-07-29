dae2d318f02491d07162635a9e8b8b4bfe3222cb
Patrick Caringal <patrickcaringalreal@gmail.com>
2020-07-29 21:15:12 +0800

    renamed repo

--- startup/index.js
+++ startup/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 require('express-async-errors');
-//
 
 module.exports = (app, rootDir) => {
     app.set('views', path.join(rootDir, 'views'));
     app.set('view engine', 'ejs');
 
+    app.use(morgan('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
