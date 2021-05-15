962f40b74a33b9fbfe3640a96a3e89d96a238124
Patrick Caringal <patrickcaringalreal@gmail.com>
2021-05-15 20:52:10 +0800

    mdb specific

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const genre = require('./genre').router;
+const movie = require('./movie');
+const people = require('./people');
+const tv = require('./tv');
+const trending = require('./trending');
 
+const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
-});
+const { NODE_ENV: env } = process.env;
 
-module.exports = router;
+module.exports = {
+    init: (app) => {
+        app.get('/', (req, res) => {
+            res.render('index', { title: process.env.APP_NAME });
+        });
+
+        if (env === 'development') {
+            app.use('/movie', require('./mock/movie'));
+            app.use('/people', require('./mock/people'));
+            app.use('/tv', require('./mock/tv'));
+        } else {
+            app.use('/movie', movie);
+            app.use('/people', people);
+            app.use('/tv', tv);
+        }
+    }
+};
--- startup/routes.js
+++ startup/routes.js
@@ -1,15 +1,14 @@
-/* eslint-disable no-console */
-const MDbRoutes = require('../routes/MDb');
+const routes = require('../routes');
 
 module.exports = (app) => {
-    MDbRoutes.init(app);
+    routes.init(app);
 
     // catch 404
     app.use((req, res) => {
         res.status(404).send('Not Found!');
     });
 
-    /* eslint-disable-next-line no-unused-vars */
+    // eslint-disable-next-line no-unused-vars
     app.use((err, req, res, next) => {
         console.log('\x1b[31m%s\x1b[0m', err);
         res.status(500).send(`Something failed. ${err.name}: ${err.message}`);
